3d3a9e97918190798634c71d6da86787d713d515
sivasundar16 <sivasundar16@gmail.com>
2017-09-19 15:31:19 +0530

    Commited first eclipse project to Git

---
+++ src/testGit/LauchSite.java
@@ -0,0 +1,18 @@
+package testGit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LauchSite {
+
+	public static void main(String[] args) {
+
+       System.setProperty("webdriver.chrome.driver", "C:\\Users\\sivasundara\\Desktop\\browserExes\\chromedriver.exe");
+       WebDriver driver=new ChromeDriver();
+       driver.get("https://stackoverflow.com/questions/20654719/push-eclipse-project-to-github-with-egit");
+       driver.close();
+       
+
+	}
+
+}
